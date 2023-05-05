0e12833a6c40053313075dea1e83a85ca8d413bf
Seake1 <christian.eriksson2@gmail.com>
2023-05-06 00:20:37 +0300

    <exciting changes>

--- server/config/express.js
+++ server/config/express.js
@@ -3,9 +3,7 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 //Setting localhost server.
 module.exports = (app) => {
-  app.use(
-    cors({ origin: "https://tori5-9online.onrender.com/", credentials: true })
-  );
+  app.use(cors({ origin: "http://localhost:3000", credentials: true }));
   app.use(express.json({ limit: "150mb" }));
   app.use(express.urlencoded({ limit: "150mb", extended: true }));
   app.use(cookieParser());
