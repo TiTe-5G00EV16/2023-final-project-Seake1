c4bae2111db5d80fa8055d92f6aa22af23c750f7
Seake1 <christian.eriksson2@gmail.com>
2023-05-06 00:03:35 +0300

    <exciting changes>

--- server/index.js
+++ server/index.js
@@ -9,7 +9,6 @@ require("./config/express")(app);
 require("./config/mongoose");
 app.use(auth());
 app.use(routes);
-http.listen(PORT);
 http.listen(PORT, () =>
   console.log(`Server is running at http://localhost:${PORT}...`)
 );
